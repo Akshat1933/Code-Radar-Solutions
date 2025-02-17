ce661e89d06de8a0e3dde584f8769ac0554add37
Akshat Sharma <akshat181818sharma@gmail.com>
2025-02-17 23:49:31 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,22 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int A,B,C,D;
+    scanf("%d %d %d %d",&A,&B,&C,&D);
+    if(A>=B && A>=C && A>=D){
+        printf("%d",A);
+    }
+    else if(B>=A && B>=C && B>=D){
+        printf("%d",B);
+    }
+    else if(C>=A && C>=B && C>=D){
+        printf("%d",C);
+    }
+    else{
+        printf("%d",D)
+    }
+    
+
+    
+
+}
