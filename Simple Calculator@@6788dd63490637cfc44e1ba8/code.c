907ae9c68ea2df7095ad601d3274c6b7c0269d4d
Akshat Sharma <akshat181818sharma@gmail.com>
2025-02-18 11:48:25 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -11,17 +11,17 @@ int main() {
     // Perform calculation based on the operator
     switch (operator) {
         case '+':
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            printf("%lf + %lf = %lf\n", num1, num2, num1 + num2);
             
         case '-':
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            printf("%lf - %lf = %lf\n", num1, num2, num1 - num2);
             
         case '*':
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            printf("%lf * %lf = %lf\n", num1, num2, num1 * num2);
             
         case '/':
             if (num2 != 0) {
-                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+                printf("%lf / %lf = %lf\n", num1, num2, num1 / num2);
             } 
             else {
                 printf("Error! Division by zero.\n");
